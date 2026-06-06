86bbcff837ad25f8851a5a3d30d41a3595e8594f
Sw3Defy <sw3defy@gmail.com>
2026-06-06 17:58:38 +0200

    Add stub scripts for unimplemented features

---
+++ scripts/git_backup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+SCRIPT_DIR=/mnt/UDISK/helper-script
+. "$SCRIPT_DIR/scripts/system.sh"
+
+install_git_backup() {
+    echo ""
+    log_info "This feature is not yet implemented for K2 Plus."
+    log_info "Please check the wiki for updates:"
+    log_info "https://sw3defy.github.io/Creality-Helper-Script-Wiki-K2-Plus/"
+    echo ""
+}
+
+remove_git_backup() {
+    echo ""
+    log_info "This feature is not yet implemented for K2 Plus."
+    echo ""
+}
+
+case "$1" in
+    install) install_git_backup ;;
+    remove)  remove_git_backup ;;
+    *)       echo "Usage: $0 [install|remove]" ;;
+esac
