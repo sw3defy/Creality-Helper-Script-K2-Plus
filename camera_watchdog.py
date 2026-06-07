10c7a17fa15a648704cc9f721dc5722b64b11dca
Sw3Defy <sw3defy@gmail.com>
2026-06-08 00:27:29 +0200

    Sync camera_watchdog.py with local version

--- camera_watchdog.py
+++ camera_watchdog.py
@@ -1,19 +1,6 @@
 #!/usr/bin/env python3
 import urllib.request, json, time
 
-def check_stream():
-    try:
-        r = urllib.request.urlopen('http://127.0.0.1:1984/api/streams', timeout=3)
-        data = json.loads(r.read())
-        stream = data.get('k2plus', {})
-        producers = stream.get('producers', [])
-        if not producers:
-            return False
-        bytes_recv = producers[0].get('bytes_recv', 0)
-        return bytes_recv > 0
-    except:
-        return False
-
 def reconnect():
     try:
         urllib.request.urlopen('http://127.0.0.1:1984/api/streams?src=k2plus', timeout=10)
@@ -22,35 +9,16 @@ def reconnect():
         print('Reconnect error:', e, flush=True)
 
 print('Camera watchdog started', flush=True)
-last_bytes = 0
-stale_count = 0
 
 while True:
     try:
         r = urllib.request.urlopen('http://127.0.0.1:1984/api/streams', timeout=3)
         data = json.loads(r.read())
-        stream = data.get('k2plus', {})
-        producers = stream.get('producers', [])
-        
+        producers = data.get('k2plus', {}).get('producers', [])
         if not producers:
             print('No producers - reconnecting...', flush=True)
             reconnect()
-            stale_count = 0
-            last_bytes = 0
-        else:
-            current_bytes = producers[0].get('bytes_recv', 0)
-            if current_bytes == last_bytes:
-                stale_count += 1
-                if stale_count >= 3:
-                    print('Stream stale - reconnecting...', flush=True)
-                    reconnect()
-                    stale_count = 0
-                    last_bytes = 0
-            else:
-                stale_count = 0
-                last_bytes = current_bytes
     except Exception as e:
         print('Watchdog error:', e, flush=True)
         reconnect()
-    
-    time.sleep(10)
+    time.sleep(30)
